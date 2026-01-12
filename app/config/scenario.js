b321ef78256df66c859f0d2434551f509429cab5
Platon <platon20081019@gmail.com>
2026-01-12 19:14:06 +0200

    I fixed the package.json  start script which was pointing to the wrong file

--- app/config/scenario.js
+++ app/config/scenario.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Target user username (without @) or phone number
-  target: "username_or_phone",
+  target: "guilemt",
 
   // Gift details
   gift: {
-    count: 5, // Total hearts to send
+    count: 1, // Total hearts to send
   },
 
   // Delays between actions (ms)
